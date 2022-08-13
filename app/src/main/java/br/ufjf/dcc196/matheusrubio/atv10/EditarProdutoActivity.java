22901cd7323657e084f23f045167eed6f5cdc892
Matheus Rubio <matheus.melo@smartnx.io>
2022-08-13 09:46:01 -0300

    Layout Cadastro e edição do produto

---
+++ app/src/main/java/br/ufjf/dcc196/matheusrubio/atv10/EditarProdutoActivity.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc196.matheusrubio.atv10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditarProdutoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_produto);
+    }
+}
